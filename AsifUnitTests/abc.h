ebd5271dd537792a5805a1fa811e53abd3bcad03
Asif Noor <asef.noor@gmail.com>
2015-11-12 10:54:47 +0500

    mbc

---
+++ AsifUnitTests/abc.h
@@ -0,0 +1,13 @@
+//
+//  abc.h
+//  AsifUnitTests
+//
+//  Created by Asif Noor on 11/12/15.
+//  Copyright © 2015 Asif Noor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface abc : NSObject
+
+@end
---
+++ AsifUnitTests/abc.m
@@ -0,0 +1,13 @@
+//
+//  abc.m
+//  AsifUnitTests
+//
+//  Created by Asif Noor on 11/12/15.
+//  Copyright © 2015 Asif Noor. All rights reserved.
+//
+
+#import "abc.h"
+
+@implementation abc
+
+@end
